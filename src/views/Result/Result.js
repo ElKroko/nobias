0a0109b57df1da10b9be5548a548046246853f74
Vicente Perelli <vicente.perelli@sansano.usm.cl>
2021-12-02 05:37:13 -0300

    pushing

--- src/views/Result/Result.js
+++ src/views/Result/Result.js
@@ -15,7 +15,7 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-
+ 
 
 import FAB from "../../Components/Fab/fab.js"
 
